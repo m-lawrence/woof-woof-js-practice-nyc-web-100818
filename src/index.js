505241c6b752284eea11d64b3c1007f3bfdd376a
Melissa Lawrence <mlawrence537@gmail.com>
2021-10-18 14:32:15 -0400

    fetch and display dog names on top bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', e => {
   const domController = new DOMController
 })
+
+const dogBar = document.querySelector('div#dog-bar')
+
+function getDogNames() {
+
+  fetch('http://localhost:3000/pups')
+    .then(r => r.json())
+    .then(dogs => dogs.forEach(dog => displayDogName(dog)))
+}
+
+function displayDogName(dogObj) {
+  const dogSpan = document.createElement('span')
+  dogSpan.dataset.id = dogObj.id
+
+  dogSpan.textContent = dogObj.name 
+
+  dogBar.append(dogSpan)
+}
+
+getDogNames()
